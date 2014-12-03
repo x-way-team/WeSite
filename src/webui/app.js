06b3cb98c96c8e68203cef901a356c46550f6d7b
leonkaihao <leonkaihao@outlook.com>
2014-12-03 02:02:33 +0000

    fix

--- src/webui/app.js
+++ src/webui/app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(session({
     secret: 'keyboard cat',
     resave: true,
-    saveUnintialized: true
+    saveUninitialized: true
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
