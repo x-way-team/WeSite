47a732f0e0dc0fa07d02a382e84c606007aea7ae
leonkaihao <leonkaihao@outlook.com>
2014-12-05 10:45:47 +0000

    start wechat

--- src/webui/app.js
+++ src/webui/app.js
@@ -5,9 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var wechat = require('wechat');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var myWechat = require('./routes/mywechat');
 
 var app = express();
 
@@ -26,11 +28,47 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(express.query());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/wechat', wechat('nanssywillcatchYOU', function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+  if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+    res.reply('hehe');
+  } else if (message.FromUserName === 'text') {
+    //你也可以这样回复text类型的信息
+    res.reply({
+      content: 'text object',
+      type: 'text'
+    });
+  } else if (message.FromUserName === 'hehe') {
+    // 回复一段音乐
+    res.reply({
+      type: "music",
+      content: {
+        title: "来段音乐吧",
+        description: "一无所有",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3"
+      }
+    });
+  } else {
+    // 回复高富帅(图文回复)
+    res.reply([
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
